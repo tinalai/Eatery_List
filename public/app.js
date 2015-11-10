8d0c490a83197e7e04eea5406279fa65a998add2
Tina Lai <etchaway@gmail.com>
2015-11-09 20:24:20 -0800

    working app, connect to firebase, add some styling

---
+++ public/app.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('app', ['firebase']);
+
+myApp.controller("MainController", [ '$scope', '$firebaseArray', function($scope, $firebaseArray){
+  var ref = new Firebase('https://blazing-torch-1958.firebaseio.com/toeats');
+
+  //create a synchronized array
+  $scope.toeats = $firebaseArray(ref);
+
+  // add new items to the array
+  // the text is automatically added to Firebase database
+  $scope.addToeats = function(){
+    $scope.toeats.$add({
+      text: $scope.newToeat,
+      user: $scope.username
+    });
+  };
+
+}]);
