2c581e60959ef59f1efe8418217b63823dcc3c26
Tina Lai <etchaway@gmail.com>
2015-11-10 16:02:16 -0800

    set up express RESTful API routes

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+/*firebase application
 var myApp = angular.module('app', ['firebase']);
 
 myApp.controller("MainController", [ '$scope', '$firebaseArray', function($scope, $firebaseArray){
+  var ref = new Firebase('https://blazing-torch-1958.firebaseio.com/toeats');
+
+  //create a synchronized array
+  $scope.toeats = $firebaseArray(ref);
+
+  // add new items to the array
+  // the text is automatically added to Firebase database
+  $scope.addToeats = function(){
+    $scope.toeats.$add({
+      text: $scope.newToeat,
+      user: $scope.username
+    });
+  };
+
+}]);
+*/
+
+var myApp = angular.module('app', []);
+
+myApp.controller("MainController", [ '$scope', '$http', function($scope, $http){
   var ref = new Firebase('https://blazing-torch-1958.firebaseio.com/toeats');
 
   //create a synchronized array
--- server.js
+++ server.js
@@ -1,6 +1,75 @@
 var app = require('./server/server-config.js');
-
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var port = process.env.PORT || 8080;
+
+var toeatSchema = new Schema({
+  text: String,
+  user: String,
+  created_at: Date
+});
+
+var ToEat = mongoose.model('ToEat', toeatSchema);
+// api
+// get all toeats
+app.get('/api/toeats', function(req, res) {
+  ToEat.find(function(err, toeats) {
+    if(err) {
+      res.send(err);
+      console.error(err);
+    }
+    res.json(toeats);
+  });
+});
+
+// create toeat and send back all toeats after creation
+app.post('api/toeats', function(req, res){
+  // create toeat, info come from AJAX request from Angular
+  ToEat.create({
+    text: req.body.text,
+    user: req.body.username,
+  }, function(err, toeat){
+    if(err){
+      res.send(err);
+      console.error(err);
+    }
+    // get and return all todos after creating another
+    ToEat.find(function(err, toeats){
+      if(err){
+        res.send(err);
+        console.error(err);
+      }
+      res.json(toeats);
+    });
+  });
+});
+
+// delete a toeat
+app.delete('/api/toeats/:toeat_id', function(req, res){
+  ToEat.remove({
+    _id : req.params.toeat_id
+  }, function(err, toeat) {
+    if(err){
+      res.send(err);
+      console.error(err);
+    }
+
+    ToEat.find(function(err, toeats) {
+      if(err){
+        res.send(err);
+        console.error(err);
+      }
+      res.json(toeats);
+    });
+  });
+});
+
+
+app.get('*', function(req, res) {
+  res.sendfile('/index.html'); // load the single page application
+})
+
+
 
 app.listen(port);
 
