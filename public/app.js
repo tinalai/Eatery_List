b2a290bc6c26612dee22cd76dbc50cd701a45c55
Tina Lai <etchaway@gmail.com>
2015-11-18 17:07:13 -0800

    delete none firebase files

--- public/app.js
+++ public/app.js
@@ -1,39 +1,19 @@
-var myApp = angular.module('app', []);
-myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
-  $scope.formData = {};
 
-  // when landing on the page, get all toeats and show toeat
-  $http.get('api/toeats')
-      .success(function(data){
-        $scope.toeats = data;
-        console.log(data);
-      })
-      .error(function(data){
-        console.log('Error: ' + data);
-      });
+var myApp = angular.module('app', ['firebase']);
 
-  // when submitting the add form, send the next to the node API
+myApp.controller("MainController", [ '$scope', '$firebaseArray', function($scope, $firebaseArray){
+  var ref = new Firebase('https://blazing-torch-1958.firebaseio.com/toeats');
+
+  //create a synchronized array
+  $scope.toeats = $firebaseArray(ref);
+
+  // add new items to the array
+  // the text is automatically added to Firebase database
   $scope.addToeats = function(){
-      $http.post('/api/toeats', $scope.formData)
-      .success(function(data) {
-        $scope.formData = {};
-        $scope.toeats = data;
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log("Error: " + data);
-      });
-    }
-
-    $scope.deleteToeat = function(id) {
-        $http.delete('/api/toeats' + id)
-        .success(function(data) {
-          $scope.todos = data;
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log("Error: " + data);
-        });
-      }
+    $scope.toeats.$add({
+      text: $scope.newToeat,
+      user: $scope.username
+    });
+  };
 
 }]);
